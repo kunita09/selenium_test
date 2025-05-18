8bcb3c301f9f54766d3b29aff8d2a27842808639
kunita09 <kunita.n@kkumail.com>
2025-05-19 02:06:05 +0700

    create login test

---
+++ login.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
+
+# สร้าง Chrome WebDriver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+# เปิดเว็บไซต์
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+# รอจน input username พร้อมใช้งาน
+input_user = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Username"]'))
+)
+input_user.send_keys("Admin")
+
+# input_pass = WebDriverWait(driver, 10).until()
+# วิธีแม่นยำและเสถียรที่สุด
+input_user = driver.find_element(By.NAME, "password")
+input_user.send_keys("admin123")
+
+login = driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
+login.click()
+
+
+time.sleep(2)
